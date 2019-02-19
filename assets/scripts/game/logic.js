5705b8782ae0f427c731a08869ef8dbaebb64f36
Charlie <charliekelly31415@gmail.com>
2019-02-18 23:21:55 -0500

    Fix player stats bug
    Was unable to process games because of confusion between game object and array of cells. Now keep functions separate based on which type of parameter they take. Also added multiple new filtering functions, array-to-square conversions, and a win-highlight method

--- assets/scripts/game/logic.js
+++ assets/scripts/game/logic.js
@@ -1,5 +1,9 @@
 'use strict'
 // game/logic.js
+//
+// IMPORTANT: Pay attention to whether function param is 'game' (a Game object),
+// 'games' (an array of Game objects), or 'cells' (an array of strings) - NOT INTERCHANGEABLE!!
+//
 
 const possibleWins = [
   [0, 1, 2],
@@ -12,7 +16,18 @@ const possibleWins = [
   [2, 4, 6]
 ]
 
-// Take array of cells
+// takes possibleWins index (result of isWin), converts cells to squares,
+// sets background color of those squares to player color
+// (complicated way of re-doing winning.checkForWin)
+const highlightWinningSquares = (winIndex, bgColor) => {
+  const win = possibleWins[winIndex]
+  for (let i = 0; i < 3; i++) {
+    const sq = indexToSquare(win[i])
+    $('#' + sq).css('background-color', bgColor)
+  }
+}
+
+/* FUNCTIONS THAT TAKE AN ARRAY OF CELLS */
 
 // Public: takes a 9-item string array game representation from API
 // if game is won by xo ('x'|'o'), return
@@ -28,6 +43,7 @@ const isWin = (cells, xo) => {
 }
 
 // Takes array of cells, returns whose turn it is, or null if every cell taken
+// (check for winner first)
 const playerTurn = (cells) => {
   if (isEmpty(cells)) {
     return 'x'
@@ -40,52 +56,117 @@ const playerTurn = (cells) => {
   }
 }
 
+// takes array of cells, returns true if no cell is empty
 const isFull = (cells) => {
   return cells.every(cell => cell !== '')
 }
 
-// Takes array of cells
+// Takes array of cells, returns true if every cell is empty
 const isEmpty = (cells) => {
   return cells.every(cell => cell === '')
 }
 
-// Takes single game
+// takes array of cells, returns true if no blanks AND no winner
+const isDraw = (cells) => {
+  if (isFull(cells)) {
+    return (isWin(cells, 'x') < 0 && isWin(cells, 'o') < 0)
+  } else return false
+}
+
+/* FUNCTIONS THAT TAKE A SINGLE GAME */
+
+// Takes single game, returns
 const isBlank = (game) => {
   return isEmpty(game.cells)
 }
 
-// Take array of games
+/* FUNCTIONS THAT TAKE AN ARRAY OF GAMES */
 
-// take array of games objects, return filtered list of games won by X
+// takes array of games, returns filtered list of games won by X
 const gamesWonByX = (games) => {
   // game : { id, cells[], over, player_x, player_o }
   return games.filter(game => {
-    return (isWin(game, 'x') >= 0)
+    return (isWin(game.cells, 'x') >= 0)
   })
 }
 
+// takes array of games, returns list of games won by O
 const gamesWonByO = (games) => {
   return games.filter(game => {
-    return (isWin(game, 'o') >= 0)
+    return (isWin(game.cells, 'o') >= 0)
   })
 }
 
+// takes array of games, returns list of blank games
 const gamesUnstarted = (games) => {
   return games.filter(isBlank)
 }
 
+// return filtered list of games not won by either player
+// if initial list is Finished Games, then result is list of ties
+// otherwise, may include incomplete games
 const gamesNotWon = (games) => {
   return games.filter(game => {
-    return (isWin(game, 'x') < 0 && isWin(game, 'o') < 0)
+    return (isWin(game.cells, 'x') < 0 && isWin(game.cells, 'o') < 0)
   })
 }
 
+// takes list of games, returns list of finished games
+// theoretically unnecessary since an API call does the same thing,
+// but sometimes that's inconvenient
+const gamesFinished = (games) => {
+  return games.filter(game => game.over === true)
+}
+
+// takes list of games, returns list of unfinished games
+const gamesUnfinished = (games) => {
+  return games.filter(game => game.over === false)
+}
+
+// takes list of games, returns list of explicitly tied games,
+// whether or not they are marked 'over' ('tied' means all squares taken & no winner)
+const gamesTied = (games) => {
+  return games.filter((game) => {
+    if (isFull(game.cells)) {
+      return (isWin(game.cells, 'x') < 0 && isWin(game.cells, 'o') < 0)
+    } else {
+      return false
+    }
+  })
+}
+
+// convert square-id (cell-0-0) to array index
+const squareIdToIndex = (squareId) => {
+  const col = parseInt(squareId[5])
+  const row = parseInt(squareId[7])
+  return (col * 3) + row
+}
+
+// return squareId from index
+const indexToSquare = (index) => {
+  const row = index % 3
+  const col = Math.trunc(index / 3)
+  return `cell-${col}-${row}`
+}
+
+// TODO: gamesUnwinnable - unfinished games that can't be won by either player
+// also, gamesWinnableByX
+
 module.exports = {
+  highlightWinningSquares,
+  isWin,
   playerTurn,
+  isFull,
   isEmpty,
+  isDraw,
   isBlank,
   gamesWonByX,
   gamesWonByO,
   gamesUnstarted,
-  gamesNotWon
+  gamesNotWon,
+  gamesFinished,
+  gamesUnfinished,
+  gamesTied,
+  squareIdToIndex,
+  indexToSquare
 }
