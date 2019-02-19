9273531fe846e147f268b4070029c6054bba53dc
Charlie <charliekelly31415@gmail.com>
2019-02-18 23:27:40 -0500

    Create themes menu
    add code for choosing a theme and then updating app. still have to create more theme images

--- assets/scripts/main/options.js
+++ assets/scripts/main/options.js
@@ -1,26 +1,69 @@
 'use strict'
 // main/options.js
 
-const events = require('./events')
-const gameUi = require('./ui')
-const getFormFields = require('../../../lib/get-form-fields')
-
-const onOptionsButton = function (event) {
-  console.log('options button clicked')
-  // TODO: show options
-}
+const themes = require('./theme.js')
+// const getFormFields = require('../../../lib/get-form-fields')
 
 const onOptionSubmit = function (event) {
   event.preventDefault()
-  const formData = getFormFields(event.target)
-  console.log('submitting options form', formData)
-  // gameUi.drawBoard()
-  // gameUi.showGameBoard()
-  gameUi.initGame()
-  events.onInitBoard()
+  // const formData = getFormFields(event.target)
+  // don't actually need formData since currently only one option,
+  // which does not require any API calls
+  const newThemeId = $('#themeSelect').val()
+  if (newThemeId !== themes.getCurrentTheme().id) {
+    changeTheme(newThemeId)
+  }
+
+  $('#modalForm').modal('hide')
+}
+
+// not used - just use HTML for now
+const loadThemeOptions = function (optionType) {
+  $('#themeSelect').html('')
+  const themeOptions = themes.availableThemes
+  themeOptions.forEach((item) => {
+    $('#themeSelect').append(themeOptionHtml(optionType, item))
+  })
+}
+
+const themeOptionHtml = function (optionType, theme) {
+  let html
+  switch (optionType) {
+    case 'select':
+      html = `<option value="${theme.short}">${theme.name}</option>`
+      break
+    case 'radio':
+      html = '<div class="form-check">'
+      html += `<input class="form-check-input" type="radio" name="theme" id="theme${theme.name}" value="${theme.short}">`
+      html += `<label class="form-check-label" for="theme${theme.name}">${theme.name}</label>`
+      html += '</div>'
+      break
+  }
+  return html
+}
+
+const changeTheme = function (themeId) {
+  console.log('changing theme: ' + themeId)
+
+  const oldTheme = themes.getCurrentTheme().className
+  $('body').removeClass(oldTheme)
+  themes.setCurrentTheme(themeId)
+  initTheme()
+}
+
+// display themed items
+const initTheme = function () {
+  const currentTheme = themes.getCurrentTheme()
+  const titleImgSrc = currentTheme.path + 'title.png'
+  $('header .page-title img').attr('src', titleImgSrc)
+  $('body').addClass(currentTheme.className)
+  $('#GameBoard .square.x img').attr('src', currentTheme.path + 'x.png')
+  $('#GameBoard .square.o img').attr('src', currentTheme.path + 'o.png')
 }
 
 module.exports = {
-  onOptionsButton,
-  onOptionSubmit
+  onOptionSubmit,
+  loadThemeOptions,
+  changeTheme,
+  initTheme
 }
