633a2ffe5f523dc84d0402fd015351d1db2f0068
Charlie <charliekelly31415@gmail.com>
2019-02-22 19:16:00 -0500

    Fix the one MVP-breaking error
    - rewrite control flow on clicking occupied square
    to avoid using an undefined variable
    - also, add user message for trying to click an empty square
    after game is over

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -57,7 +57,11 @@ const onClickSquare = function (event) {
       // logged in, but need to start a new game
       $('#resetBtn').text('New Game')
       utils.userMessage('Click the "New Game" Button to start a new game')
-    } else if (square.has('img')) {
+      // } else if (square.has('img')) { // source of the one MVP-breaking error
+    } else if ($('#gameStatus').text() === 'Over') {
+      // game is over
+      utils.userMessage('The game is over. Click the "New Game" button to play again')
+    } else if ($(event.target).has('img')) {
       // square is already taken
       utils.warningMessage('That square is already taken, please pick another square')
     } else {
